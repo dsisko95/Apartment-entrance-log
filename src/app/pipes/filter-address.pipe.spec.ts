cee62314d4bf26e27d534c7551f16baa4e533585
Daniel Sisovic <sisovic7@gmail.com>
2018-12-11 10:39:26 +0100

    Removed unnecessary files

--- src/app/pipes/filter-address.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterAddressPipe } from './filter-address.pipe';
-
-describe('FilterAddressPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterAddressPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
